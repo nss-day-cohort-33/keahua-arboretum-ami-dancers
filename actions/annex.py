a6511499dc67dde78354f36f21f9667aa34ba13a
Shane Miller <shanemiller89@gmail.com>
2019-09-09 15:24:52 -0500

    added biome options

--- actions/annex.py
+++ actions/annex.py
@@ -1,5 +1,10 @@
 import os
 from environments.complete_environments import River
+from environments.complete_environments import Swamp
+from environments.complete_environments import Coastline
+from environments.complete_environments import Grassland
+from environments.complete_environments import Mountain
+from environments.complete_environments import Forest
 
 
 def annex_habitat(arboretum):
@@ -8,6 +13,9 @@ def annex_habitat(arboretum):
     print("2. Swamp")
     print("3. Coastline")
     print("4. Grassland")
+    print("5. Mountain")
+    print("6. Forest")
+    
 
     choice = input("Choose your habitat > ")
 
@@ -15,4 +23,18 @@ def annex_habitat(arboretum):
         river = River()
         arboretum.rivers.append(river)
     if choice == "2":
-        pass
+        swamp = Swamp()
+        arboretum.swamps.append(swamp)
+    if choice == "3":
+        coastline = Coastline()
+        arboretum.coastlines.append(coastline)
+    if choice == "4":
+        grassland = Grassland()
+        arboretum.grasslands.append(grassland)
+    if choice == "5":
+        mountain = Mountain()
+        arboretum.mountains.append(mountain)
+    if choice == "6":
+        forest = Forest()
+        arboretum.forests.append(forest)
+    
