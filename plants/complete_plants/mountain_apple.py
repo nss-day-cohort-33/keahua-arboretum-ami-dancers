18bee12bcf23776f0f965e4b6a482273e555d546
Tyler Carpenter <tcarp55@gmail.com>
2019-09-10 14:47:31 -0500

    add plants to biomes is done

--- plants/complete_plants/mountain_apple.py
+++ plants/complete_plants/mountain_apple.py
@@ -1,8 +1,11 @@
 from plants import Plant
 from interfaces.type import IRocky
+from interfaces import Identifiable
 
-class MountainApple(Plant, IRocky):
+
+class MountainApple(Plant, IRocky, Identifiable):
 
     def __init__(self):
         Plant.__init__(self, "Mountain Apple Tree", "Partial", 17, "high")
         IRocky.__init__(self)
+        Identifiable.__init__(self)
--- plants/complete_plants/silversword.py
+++ plants/complete_plants/silversword.py
@@ -1,8 +1,11 @@
 from plants import Plant
 from interfaces.type import IFlat
+from interfaces import Identifiable
 
-class Silversword(Plant):
+
+class Silversword(Plant, IFlat, Identifiable):
 
     def __init__(self):
-        Plant.__init__("Silversword", "Shade", 22, "high")
+        Plant.__init__(self, "Silversword", "Shade", 22, "high")
         IFlat.__init__(self)
+        Identifiable.__init__(self)
