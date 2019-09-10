4ebc83bcaad2ea45b6fa03b758d24c909b231443
Tyler Carpenter <tcarp55@gmail.com>
2019-09-10 09:40:28 -0500

    basic release animal

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -1,11 +1,24 @@
 from animals import RiverDolphin
+from animals import Opeapea
+from animals import Pueo
+from animals import Gecko
+from animals import Ulae
+from animals import Goose
+from animals import Kikakapu
+from animals import Spider
 
 
 def release_animal(arboretum):
     animal = None
 
     print("1. River Dolphin")
-    print("2. Dragonfly")
+    print("2. Opeapea")
+    print("3. Peuo")
+    print("4. Gold Dust Day Gecko")
+    print("5. Ulae")
+    print("6. Nene Goose")
+    print("7. Kikakapu")
+    print("8. Hawaiian Happy Face Spider")
 
     choice = input("Choose animal to release > ")
 
@@ -13,7 +26,25 @@ def release_animal(arboretum):
         animal = RiverDolphin()
 
     if choice == "2":
-        pass
+        animal = Opeapea()
+
+    if choice == "3":
+        animal = Pueo()
+
+    if choice == "4":
+        animal = Gecko()
+
+    if choice == "5":
+        animal = Ulae()
+
+    if choice == "6":
+        animal = Goose()
+
+    if choice == "7":
+        animal = Kikakapu()
+
+    if choice == "8":
+        animal = Spider()
 
     for index, river in enumerate(arboretum.rivers):
         print(f'{index + 1}. River {river.id}')
